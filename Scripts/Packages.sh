ed1010d87a4d5a0524805ac312327182bda92972
windyjdk <114653524+windyjdk@users.noreply.github.com>
2026-05-29 07:03:23 +0800

    Update Packages.sh

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -79,7 +79,7 @@ UPDATE_PACKAGE() {
 #UPDATE_PACKAGE "timecontrol" "sirpdboy/luci-app-timecontrol" "main"
 #UPDATE_PACKAGE "viking" "VIKINGYFY/packages" "main" "" "gecoosac luci-app-timewol luci-app-wolplus"
 #UPDATE_PACKAGE "vnt" "lmq8267/luci-app-vnt" "main"
-UPDATE_PACKAGE  "lucky" "gdy666/luci-app-lucky" "main"
+UPDATE_PACKAGE  "lucky" "gdy666/luci-app-lucky" "main" "" "previews"
 
 #更新软件包版本
 UPDATE_VERSION() {
